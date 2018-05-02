35918b50a31e0eca464be784e4c1e1961685beef
Jay Daverth <Jason.Daverth@wholefoods.com>
2018-05-01 22:04:16 -0500

    Completed RNA Transcription exercise

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,26 @@
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        String rnaStrand = "";
 
+        for (int i = 0; i < dnaStrand.length(); i++) {
+	        char strand = dnaStrand.charAt(i);
+	        switch (strand) {
+	        	case 'G': rnaStrand += 'C'; 
+	        	break;
+
+	        	case 'C': rnaStrand += 'G'; 
+	        	break;
+	        	
+	        	case 'T': rnaStrand += 'A'; 
+	        	break;
+	        	
+	        	case 'A': rnaStrand += 'U'; 
+	        	break;
+	        }
+	     }
+
+    return rnaStrand;
+
+    }
 }
