35bb2a5235ed1c1015fbc65055e967d638509461
Jay Daverth <Jason.Daverth@wholefoods.com>
2018-05-02 12:04:18 -0500

    Completed Pangram exercise

--- pangram/src/main/java/PangramChecker.java
+++ pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,23 @@
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        // evalString ordered by frequency for performance reasons
+    	char[] evalString = new char[] { 
+    		'e', 'a', 'r', 'i', 'o', 't', 'n', 's', 'l', 
+    		'c', 'u', 'd', 'p', 'm', 'h', 'g', 'b', 'f', 
+    		'y', 'w', 'k', 'v', 'x', 'z', 'j', 'q' 
+    	};
+
+    	// Hack for case insensitivity
+    	input = input.toLowerCase();
+    	
+    	for (int i=0; i<=24; i++) {
+    		if (input.indexOf(evalString[i]) < 0 ) {
+    			return false;
+    		}
+    	}
+
+	    return true;
     }
 
 }
