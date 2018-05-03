ddd25145a87cfe111bdb92a7606486c2c1142fd4
Jay Daverth <Jason.Daverth@wholefoods.com>
2018-05-03 12:59:01 -0500

    Updated to reverse frequency order and correct number letters in alphabet

--- pangram/src/main/java/PangramChecker.java
+++ pangram/src/main/java/PangramChecker.java
@@ -3,15 +3,15 @@ public class PangramChecker {
     public boolean isPangram(String input) {
         // evalString ordered by frequency for performance reasons
     	char[] evalString = new char[] { 
-    		'e', 'a', 'r', 'i', 'o', 't', 'n', 's', 'l', 
-    		'c', 'u', 'd', 'p', 'm', 'h', 'g', 'b', 'f', 
-    		'y', 'w', 'k', 'v', 'x', 'z', 'j', 'q' 
+            'q', 'j', 'z', 'x', 'v', 'k', 'w', 'y','f',
+            'b', 'g', 'h', 'm', 'p', 'd', 'u', 'c', 'l',
+            's', 'n', 't', 'o', 'i', 'r', 'a', 'e' 
     	};
 
     	// Hack for case insensitivity
     	input = input.toLowerCase();
     	
-    	for (int i=0; i<=24; i++) {
+    	for (int i=0; i<26; i++) {
     		if (input.indexOf(evalString[i]) < 0 ) {
     			return false;
     		}
